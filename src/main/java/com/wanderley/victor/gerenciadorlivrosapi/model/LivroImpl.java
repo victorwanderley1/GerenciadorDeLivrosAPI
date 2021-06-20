2f9fd0d7c136315eef4b6587ecddc82f8c892809
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-06-20 16:36:18 -0300

    Criando interface e implementação da classe Livro

---
+++ src/main/java/com/wanderley/victor/gerenciadorlivrosapi/model/LivroImpl.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wanderley.victor.gerenciadorlivrosapi.model;
+
+/**
+ *
+ * @author Victor Wanderley <wanderley.victor>
+ */
+public class LivroImpl implements Livro{
+    private Integer id;
+    private String titulo;
+    private Autor autor;
+    private Categoria categoria;
+
+    public LivroImpl() {
+        this.id = 0;
+        this.titulo = "";
+        this.autor = new AutorImpl();
+        this.categoria = new CategoriaImpl();
+    }
+
+    public LivroImpl(String titulo, Integer idAutor, Integer idCategoria) {
+        this.id = 0;
+        this.titulo = titulo;
+        this.autor = new AutorImpl(idAutor);
+        this.categoria = new CategoriaImpl(idCategoria);
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public Autor getAutor() {
+        return autor;
+    }
+
+    @Override
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    
+}
