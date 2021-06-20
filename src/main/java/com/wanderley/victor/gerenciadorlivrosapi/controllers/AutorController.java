ef7065b0e391943a120ff13227a1f996857fc198
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-06-20 17:25:11 -0300

    Criando Controller para os Autores

---
+++ src/main/java/com/wanderley/victor/gerenciadorlivrosapi/controllers/AutorController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wanderley.victor.gerenciadorlivrosapi.controllers;
+
+import com.wanderley.victor.gerenciadorlivrosapi.model.Autor;
+import com.wanderley.victor.gerenciadorlivrosapi.services.AutorService;
+import com.wanderley.victor.gerenciadorlivrosapi.services.AutorServiceImpl;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Victor Wanderley <wanderley.victor>
+ */
+@RestController
+@RequestMapping("/autor")
+public class AutorController {
+    @Autowired
+    AutorService autorService = new AutorServiceImpl();
+    
+    @GetMapping
+    ResponseEntity<List<Autor>> findAll() {
+        return new ResponseEntity(autorService.findAll(), HttpStatus.OK);
+    }
+    
+    
+    
+}
