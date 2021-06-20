d001f97899f68d18eaeba4638cb6cd8765ad9c70
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-06-20 17:29:58 -0300

    Criando Classe Application

---
+++ src/main/java/com/wanderley/victor/gerenciadorlivrosapi/GerenciadorlivrosapiApplication.java
@@ -0,0 +1,13 @@
+package com.wanderley.victor.gerenciadorlivrosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorlivrosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorlivrosapiApplication.class, args);
+	}
+
+}
