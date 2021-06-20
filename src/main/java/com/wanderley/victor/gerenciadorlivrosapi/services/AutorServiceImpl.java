14d35b40b19bcfa1ae07e50e0171a4cd0ea163e8
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-06-20 17:25:58 -0300

    Criando Interface e implementação Services para os Autores

---
+++ src/main/java/com/wanderley/victor/gerenciadorlivrosapi/services/AutorServiceImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wanderley.victor.gerenciadorlivrosapi.services;
+
+import com.wanderley.victor.gerenciadorlivrosapi.model.Autor;
+import com.wanderley.victor.gerenciadorlivrosapi.repositories.AutorRepository;
+import com.wanderley.victor.gerenciadorlivrosapi.repositories.AutorRepositoryImpl;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Victor Wanderley <wanderley.victor>
+ */
+@Service
+public class AutorServiceImpl implements AutorService {
+    
+    @Autowired
+    AutorRepository autorRepository = new AutorRepositoryImpl();
+    @Override
+    public List<Autor> findAll(){
+        return autorRepository.findAll();
+    }
+}
