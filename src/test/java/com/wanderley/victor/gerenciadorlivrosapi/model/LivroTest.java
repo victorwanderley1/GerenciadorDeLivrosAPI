d8d336331f3b50d6c06e964f6d617b915e295101
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-06-20 16:35:12 -0300

    Criando testes para classe Livro

---
+++ src/test/java/com/wanderley/victor/gerenciadorlivrosapi/model/LivroTest.java
@@ -0,0 +1,91 @@
+package com.wanderley.victor.gerenciadorlivrosapi.model;
+
+import com.wanderley.victor.gerenciadorlivrosapi.model.LivroImpl;
+import com.wanderley.victor.gerenciadorlivrosapi.model.AutorImpl;
+import com.wanderley.victor.gerenciadorlivrosapi.model.Categoria;
+import com.wanderley.victor.gerenciadorlivrosapi.model.CategoriaImpl;
+import com.wanderley.victor.gerenciadorlivrosapi.model.Autor;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Victor Wanderley <wanderley.victor>
+ */
+public class LivroTest {
+    
+    public LivroTest() {
+    }
+
+    /**
+     * Test of getId method, of class LivroImpl.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        Livro instance = new LivroImpl();
+        Integer expResult = 0;
+        Integer result = instance.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setId method, of class LivroImpl.
+     */
+    @Test
+    public void testSetId() {
+        System.out.println("setId");
+        Integer id = 3;
+        Livro instance = new LivroImpl();
+        instance.setId(id);
+    }
+
+    /**
+     * Test of getTitulo method, of class LivroImpl.
+     */
+    @Test
+    public void testGetTitulo() {
+        System.out.println("getTitulo");
+        Livro instance = new LivroImpl();
+        String expResult = "";
+        String result = instance.getTitulo();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setTitulo method, of class LivroImpl.
+     */
+    @Test
+    public void testSetTitulo() {
+        System.out.println("setTitulo");
+        String titulo = "algum título";
+        Livro instance = new LivroImpl();
+        instance.setTitulo(titulo);
+        
+    }
+
+    /**
+     * Test of getAutor method, of class LivroImpl.
+     */
+    @Test
+    public void testGetAutor() {
+        System.out.println("getAutor");
+        Livro instance = new LivroImpl();
+        Autor expResult = new AutorImpl();
+        Autor result = instance.getAutor();
+        assertEquals(expResult.getId(), result.getId());
+    }
+
+    /**
+     * Test of getCategoria method, of class LivroImpl.
+     */
+    @Test
+    public void testGetCategoria() {
+        System.out.println("getCategoria");
+        Livro instance = new LivroImpl();
+        Categoria expResult = new CategoriaImpl();
+        Categoria result = instance.getCategoria();
+        assertEquals(expResult.getId(), result.getId());
+    }
+    
+}
