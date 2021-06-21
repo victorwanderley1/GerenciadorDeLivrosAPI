d7954492cb15b110505266127f2c2241e5a775ac
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-06-21 16:32:30 -0300

    Criando testes para a interface e implementação AutorRepository

---
+++ src/test/java/com/wanderley/victor/gerenciadorlivrosapi/repositories/AutorRepositoryTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wanderley.victor.gerenciadorlivrosapi.repositories;
+
+import com.wanderley.victor.gerenciadorlivrosapi.model.Autor;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Victor Wanderley <wanderley.victor>
+ */
+public class AutorRepositoryTest {
+    
+    /**
+     * Test of findAll method, of class AutorRepository.
+     */
+    @Test
+    public void testFindAll() {
+        System.out.println("findAll");
+        AutorRepository instance = new AutorRepositoryImpl();
+        List<Autor> expResult = null;
+        List<Autor> result = instance.findAll();
+        assertTrue(expResult.addAll(result));
+    }
+
+    
+}
