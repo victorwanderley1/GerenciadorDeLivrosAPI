00e80b609effc27b99a53aa5d31aa517779f63cb
victorwanderley1 <65874249+victorwanderley1@users.noreply.github.com>
2021-06-25 16:06:58 -0300

    Teste Application

---
+++ src/test/java/com/wanderley/victor/gerenciadorlivrosapi/GerenciadorlivrosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderley.victor.gerenciadorlivrosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorlivrosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
